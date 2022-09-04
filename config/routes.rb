b6a66cd448e332b86ca35b6a29a6cd2aa3d015f2
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-09-04 14:28:23 +0000

    [Add]users_controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   get 'homes/top'
   get 'homes/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
