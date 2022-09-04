4f9f27553abc18d2f91ad8838f70f08828106036
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-09-04 15:00:24 +0000

    [Add]stock_modelの作成

---
+++ db/migrate/20220904144432_create_stocks.rb
@@ -0,0 +1,15 @@
+class CreateStocks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :stocks do |t|
+      t.string :name,         null: false
+      t.date :purchase_date,  null: false
+      t.integer :percentage,  null: false
+      t.integer :stock_amount,null: false
+      t.integer :category_id, null: false
+      t.datetime :alert_date, null: false
+      t.integer :user_id,     null: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_04_143732) do
+ActiveRecord::Schema.define(version: 2022_09_04_144432) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "name", null: false
+    t.date "purchase_date", null: false
+    t.integer "percentage", null: false
+    t.integer "stock_amount", null: false
+    t.integer "category_id", null: false
+    t.datetime "alert_date", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
