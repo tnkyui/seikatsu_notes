9f1982d2bf5a9f1cdabf16b06c99d9b73939f76f
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-09-07 17:40:01 +0000

    [Add]stock登録機能の大枠

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,16 +1,27 @@
 class StocksController < ApplicationController
   def index
+    @stock = Stock.new
+    @category = Category.new
   end
 
   def show
   end
 
   def create
+    stock = Stock.new(stock_params)
+    stock.save
+    redirect_to stocks_path
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def stock_params
+    params.require(:stock).permit(:name, :purchase_date, :percentage, :stock_amount, :alert_date)
   end
 end
