0c1e9a39c3db955ba605316f28ed0614c0086704
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-09-08 16:09:39 +0000

    [Add]lists新規登録機能作成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,16 @@
 class ListsController < ApplicationController
   def index
+    @shopping_list = ShoppingList.new
+    @todo_list = TodoList.new
   end
 
   def create
+    shopping_list = ShoppingList.new(shopping_list_params)
+    shopping_list.save
+    redirect_to lists_path
+    todo_list = TodoList.new(todo_list_params)
+    todo_list.save
+    redirect_to lists_path
   end
 
   def update
@@ -10,4 +18,13 @@ class ListsController < ApplicationController
 
   def destroy
   end
+
+  private
+    def shopping_list_params
+      params.require(:shopping_list).permit(:name)
+    end
+
+    def todo_list_params
+      params.require(:todo_list).permit(:name, :start_time)
+    end
 end
