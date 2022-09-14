c2bc0057a77db71e8c4913c761b04248d62f5e83
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-09-14 16:39:34 +0000

    [Fix]before_actionの削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,6 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    home_path
-  end
-
-  def after_sign_up_path_for(resource)
     home_path
   end
 
