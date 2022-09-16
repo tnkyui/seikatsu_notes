1115d71152de786dc6f1fd4a6247e33dc9d30b34
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-09-16 16:51:22 +0000

    [Add]home add_to_shopping_list?の雛形を作成

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,6 +6,9 @@ class HomesController < ApplicationController
     user = current_user
     todo_lists = user.todo_lists.all
     stocks = user.stocks.all
+    shopping_lists = user.shopping_lists.all
     @events = todo_lists | stocks
+    @stock = user.stocks.where(percentage: 0..20, stock_amount: 0)
+    @shopping_list = ShoppingList.new
   end
 end
