245501d24dc507ebfcc6bf83e5f83a80ec8f802c
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-09-17 15:41:18 +0000

    [Add]home_alert機能の実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -7,8 +7,13 @@ class HomesController < ApplicationController
     todo_lists = user.todo_lists.all
     stocks = user.stocks.all
     shopping_lists = user.shopping_lists.all
+    # 以下カレンダー用
     @events = todo_lists | stocks
-    @stock = user.stocks.where(percentage: 0..20, stock_amount: 0)
+    # 以下Add_shopping_list?用
+    @stock_add_list = user.stocks.where(percentage: 0..20, stock_amount: 0)
     @shopping_list = ShoppingList.new
+    # 以下alert用
+    @stock_alert = user.stocks.where(alert_switch: true).where("alert_date <= ?", Date.current)
+    # @stock = Stock.find(params[:id])
   end
 end
