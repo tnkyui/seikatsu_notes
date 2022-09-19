96fa2fbc6bc138f3934060748d15545398553d71
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-09-19 16:12:50 +0000

    [Update]home layoutsの修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,10 +5,10 @@ class HomesController < ApplicationController
   def index
     user = current_user
     todo_lists = user.todo_lists.all
-    stocks = user.stocks.all
+    @stocks = user.stocks.where(alert_switch: true)
     shopping_lists = user.shopping_lists.all
     # 以下カレンダー用
-    @events = todo_lists | stocks
+    @events = todo_lists | @stocks
     # 以下Add_shopping_list?用
     @stock_add_list = user.stocks.where(percentage: 0..20, stock_amount: 0)
     @shopping_list = ShoppingList.new
