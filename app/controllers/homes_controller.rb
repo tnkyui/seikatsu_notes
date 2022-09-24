9dc2af949cc32750f8c76e010df6dbfb4fa89e06
EC2 Default User <ec2-user@ip-172-31-43-237.ec2.internal>
2022-09-24 16:57:59 +0000

    [Update]home add_shopping_listリスト追加済みを非表示

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -10,8 +10,9 @@ class HomesController < ApplicationController
     # 以下カレンダー用
     @events = todo_lists | @stocks
     # 以下Add_shopping_list?用
-    @stock_add_list = user.stocks.where(percentage: 0..20, stock_amount: 0)
-    @shopping_list = ShoppingList.new
+    stock_add_list = user.stocks.where(percentage: 0..20, stock_amount: 0).pluck(:name)
+    @stock_add_list = stock_add_list - shopping_lists.pluck(:name)
+    @shopping_list = ShoppingList.new #Addボタン用
     # 以下alert用
     @stock_alert = user.stocks.where(alert_switch: true).where("alert_date <= ?", Date.current)
   end
