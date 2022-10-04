a13ec45ad85e01ce27321fe25ac90a0b2c184c63
tnkyui <nkizrhjktsm.ue10@gmail.com>
2022-10-04 17:59:03 +0000

    [Add]list complete_buttonの実装

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,11 +4,11 @@ class HomesController < ApplicationController
 
   def index
     user = current_user
-    todo_lists = user.todo_lists.all
+    @todo_lists = user.todo_lists.all
     @stocks = user.stocks.where(alert_switch: true)
     shopping_lists = user.shopping_lists.all
     # 以下カレンダー用
-    @events = todo_lists | @stocks
+    @events = @todo_lists | @stocks
     # 以下Add_shopping_list?用
     stock_add_list = user.stocks.where(percentage: 0..20, stock_amount: 0).pluck(:name)
     @stock_add_list = stock_add_list - shopping_lists.pluck(:name)
--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -4,24 +4,24 @@ class TodoListsController < ApplicationController
     @todo_list = TodoList.new(todo_list_params)
     @todo_list.user_id = current_user.id
     @todo_list.save
-    redirect_to lists_path
+    redirect_to request.referer
   end
 
   def update
     todo_list = TodoList.find(params[:id])
-    todo_list.update
-    redirect_to lists_path
+    todo_list.update(todo_list_params)
+    redirect_to request.referer
   end
 
   def destroy
     todo_list = TodoList.find(params[:id])
     todo_list.destroy
-    redirect_to lists_path
+    redirect_to request.referer
   end
 
   private
     def todo_list_params
-      params.require(:todo_list).permit(:name, :start_date)
+      params.require(:todo_list).permit(:name, :start_date, :complete_switch)
     end
 
 end
