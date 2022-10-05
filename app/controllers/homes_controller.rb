265a01a4c377093db5376aa93ecaf3ed07893276
tnkyui <nkizrhjktsm.ue10@gmail.com>
2022-10-05 17:22:54 +0000

    [Update]home add_shopping_list?のlinkを復活

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -11,7 +11,10 @@ class HomesController < ApplicationController
     @events = @todo_lists | @stocks
     # 以下Add_shopping_list?用
     stock_add_list = user.stocks.where(percentage: 0..20, stock_amount: 0).pluck(:name)
-    @stock_add_list = stock_add_list - shopping_lists.pluck(:name)
+    @stock_add_list = stock_add_list - shopping_lists.pluck(:name) #かうものリストと重複するものは省く
+      @stock_add_list.each do |stock|
+        @stock_id = user.stocks.where(name: stock) #link用に配列からデータを再度引っ張る
+      end
     @shopping_list = ShoppingList.new #Addボタン用
     # 以下alert用
     @stock_alert = user.stocks.where(alert_switch: true).where("alert_date <= ?", Date.current)
