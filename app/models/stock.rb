7147cb8c5f26d1972c436823679be5e2d2aa39b7
tnkyui <nkizrhjktsm.ue10@gmail.com>
2022-10-05 07:41:21 +0000

    [Add]category enum setting

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,9 @@
 class Stock < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  
+
+  enum category: { meats_fish: 0, vegetables: 1, other_food: 2, drink: 3, kitchen_tools: 4, cleaning: 5, beauty: 6, daily_necessities: 7, other: 8, not_set: 9 }
+
   def start_time
     self.alert_date.to_time.to_datetime
   end
