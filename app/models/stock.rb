2c3d79e778b6bd2ae81bb064d90a93514ce467f2
tnkyui <nkizrhjktsm.ue10@gmail.com>
2022-10-05 08:14:05 +0000

    [Add]category enum 項目修正

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,8 @@ class Stock < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  enum category: { meats_fish: 0, vegetables: 1, spices: 2, drink: 3, kitchen_tools: 4, cleaning: 5, beauty: 6, daily_necessities: 7, other: 8, not_set: 9 }
+  enum category: { meats_fish: 0, vegetables: 1, spices: 2, drink: 3, other_food: 30,kitchen_tools: 40, cleaning: 50, beauty: 60, daily_necessities: 70, other: 98, not_set: 99 }
+  # 今後増加したときのために数字の間隔を空けています
 
   def start_time
     self.alert_date.to_time.to_datetime
