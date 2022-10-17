f7337fa4d362c20232334c3d67258f3788a8b6e7
tnkyui <nkizrhjktsm.ue10@gmail.com>
2022-10-17 14:09:58 +0000

    [Add]user_information/退会機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
   def edit
+    @user = current_user
   end
 
   def update
   end
 
   def destroy
+    user = User.find(current_user.id)
+    user.destroy
+    redirect_to root_path
   end
 end
