04dc31d196a0cdbef815829cb501811bb78d2569
tnkyui <nkizrhjktsm.ue10@gmail.com>
2022-10-18 16:56:43 +0000

    [Add]todo_list repeat機能実装

--- app/controllers/todo_lists_controller.rb
+++ app/controllers/todo_lists_controller.rb
@@ -8,8 +8,16 @@ class TodoListsController < ApplicationController
   end
 
   def update
-    todo_list = TodoList.find(params[:id])
-    todo_list.update(todo_list_params)
+    @todo_list = TodoList.find(params[:id])
+    if params[:todo_list][:repeat_days] != "0" && params[:todo_list][:complete_switch] == "true"
+      repeat_todo_list = TodoList.new
+      repeat_todo_list.name = @todo_list.name
+      repeat_todo_list.user_id = current_user.id
+      repeat_todo_list.start_date = @todo_list.start_date + params[:todo_list][:repeat_days].to_i
+      repeat_todo_list.repeat_days = @todo_list.repeat_days
+      repeat_todo_list.save
+    end
+    @todo_list.update(todo_list_params)
     redirect_to request.referer
   end
 
@@ -21,7 +29,7 @@ class TodoListsController < ApplicationController
 
   private
     def todo_list_params
-      params.require(:todo_list).permit(:name, :start_date, :complete_switch)
+      params.require(:todo_list).permit(:name, :start_date, :complete_switch, :repeat_days)
     end
 
 end
