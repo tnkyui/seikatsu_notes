d235bbe8e1a481b32154927e10a42de184f5a65d
tnkyui <nkizrhjktsm.ue10@gmail.com>
2022-10-29 15:34:01 +0000

    [Fix]kaminari tab切替でページが反映してしまう修正

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,18 +1,18 @@
 class StocksController < ApplicationController
   def index
     @stock = Stock.new
-    @stocks = current_user.stocks.page(params[:page]).per(10)
-    @stock_meats_fish = @stocks.where(category: 0).page(params[:page]).per(10)
-    @stock_vegetables = @stocks.where(category: 1).page(params[:page]).per(10)
-    @stock_spices = @stocks.where(category: 2).page(params[:page]).per(10)
-    @stock_drink = @stocks.where(category: 3).page(params[:page]).per(10)
-    @stock_other_food = @stocks.where(category: 30).page(params[:page]).per(10)
-    @stock_kitchen_tools = @stocks.where(category: 40).page(params[:page]).per(10)
-    @stock_cleaning = @stocks.where(category: 50).page(params[:page]).per(10)
-    @stock_beauty = @stocks.where(category: 60).page(params[:page]).per(10)
-    @stock_daily_necessities = @stocks.where(category: 70).page(params[:page]).per(10)
-    @stock_other = @stocks.where(category: 98).page(params[:page]).per(10)
-    @stock_not_set = @stocks.where(category: 99).page(params[:page]).per(10)
+    @stocks = current_user.stocks.page(params[:stocks_page]).per(10)
+    @stock_meats_fish = @stocks.where(category: 0).page(params[:meats_fish_page]).per(10)
+    @stock_vegetables = @stocks.where(category: 1).page(params[:vegetables_page]).per(10)
+    @stock_spices = @stocks.where(category: 2).page(params[:spices_page]).per(10)
+    @stock_drink = @stocks.where(category: 3).page(params[:drink_page]).per(10)
+    @stock_other_food = @stocks.where(category: 30).page(params[:other_food_page]).per(10)
+    @stock_kitchen_tools = @stocks.where(category: 40).page(params[:kitchen_tools_page]).per(10)
+    @stock_cleaning = @stocks.where(category: 50).page(params[:cleaning_page]).per(10)
+    @stock_beauty = @stocks.where(category: 60).page(params[:beauty_page]).per(10)
+    @stock_daily_necessities = @stocks.where(category: 70).page(params[:daily_necessities_page]).per(10)
+    @stock_other = @stocks.where(category: 98).page(params[:other_page]).per(10)
+    @stock_not_set = @stocks.where(category: 99).page(params[:not_set_page]).per(10)
     @shopping_list = ShoppingList.new
     # 以下リストへ追加ボタン
     @shopping_list_name = current_user.shopping_lists.pluck(:name)
@@ -27,7 +27,7 @@ class StocksController < ApplicationController
     stock.user_id = current_user.id
     in_list = current_user.shopping_lists.find_by(name: params[:stock][:name])
     in_stock = current_user.stocks.find_by(name: params[:stock][:name])
-    if in_list.present?  #もし買い物リストあるものを購入したら
+    if in_list.present?  #もし買い物リストに既にあるものを購入したら
       in_list.destroy
     end
     if in_stock.present?  #もしｽﾄｯｸに同じ名称の在庫があったら
