480bb7978f6e00650e07e5df0c941895a38f6424
tnkyui <nkizrhjktsm.ue10@gmail.com>
2022-11-07 15:48:18 +0000

    [fix]list pagenate_fix

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,9 +3,9 @@ class ListsController < ApplicationController
   def index
     @user = current_user
     @shopping_list = ShoppingList.new
-    @shopping_lists = @user.shopping_lists.page(params[:page]).per(10)
+    @shopping_lists = @user.shopping_lists.page(params[:shopping_page]).per(10)
     @todo_list = TodoList.new
-    @todo_lists = @user.todo_lists.order(:complete_switch).order(:start_date).page(params[:page]).per(10)
+    @todo_lists = @user.todo_lists.order(:complete_switch).order(:start_date).page(params[:todo_page]).per(10)
   end
 
 end
